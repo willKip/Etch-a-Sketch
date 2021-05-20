63810329a2f15211b870844bab68667e77218284
willKip <coldbrushl@gmail.com>
2021-05-20 11:53:04 -0400

    Add size change, reset functionality
    The board may now be wiped clean with the reset button,
    or change its size anywhere from 1x1 to 100x100 with the
    change size button.

--- etch.js
+++ etch.js
@@ -1,28 +1,45 @@
-function initTiles () {
-    const GRID_SIZE = 16;
+function initTiles (gridSize) {
+    // handle out of range sizes; over 100 risks performance issues
+    if (gridSize > 100) gridSize = 100;
+    else if (gridSize <= 0) gridSize = 1;
 
-    for (let x = 0; x < GRID_SIZE**2; x++) {
+    let board = document.querySelector(".container");
+
+    // Clear tiles from board
+    while (board.firstChild) board.removeChild(board.firstChild);
+
+    board.style.setProperty("--tileSize", gridSize)
+
+    for (let x = 0; x < gridSize**2; x++) {
         let tile = document.createElement("div");
         tile.className = "tile";
 
-        document.querySelector(".container").appendChild(tile);
+        board.appendChild(tile);
         tile.addEventListener("mouseover", () => {
             tile.classList.add("filled");
         });
     }
 }
 
-function resetBoard() {
+function unfillBoard() {
     let tiles = document.getElementsByClassName("tile");
-    for (let i=0; i < tiles.length; i++) {
+    for (let i=0; i < tiles.length; i++)
         tiles[i].classList.remove("filled");
-    }
-    console.log("reset");
+    console.log("unfilled");
 }
 
-const resetBtn = document.querySelector("#resetButton");
-resetBtn.addEventListener("click", ()=>{
-    resetBoard();
-});
+function changeGridSize() {
+    let newSize = prompt("Enter the new grid size (1-100)", "16");
+    initTiles(newSize);
+}
 
-initTiles();
+function initGame(initSize) {
+    document.querySelector("#resetButton").addEventListener(
+        "click", ()=>{unfillBoard();});
+    document.querySelector("#changeGridSize").addEventListener(
+        "click", ()=>{changeGridSize();});
+
+    initTiles(initSize);
+}
+
+initGame(16);
